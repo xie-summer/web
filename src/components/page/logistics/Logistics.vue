6b9cdda7a45c169e9a0ddb6a1fe594608e7fa902
zhangming <zhangming@newhope-data.com>
2017-12-21 16:04:35 +0800

    在途物资新增时间选项，增点卡片样式

--- src/components/page/logistics/Logistics.vue
+++ src/components/page/logistics/Logistics.vue
@@ -1,50 +1,71 @@
 <template>
-    <div class="back">
+    <div class="back" style="min-width: 1055px">
         <div class="crumbs" style="margin-bottom: 20px">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item><i class="el-icon-date"></i> 在途资产</el-breadcrumb-item>
 
             </el-breadcrumb>
         </div>
+        <el-row style="margin-bottom: 3rem">
+            <el-col :span="4" :offset="20">
+                <div style="float: right">
+                    <el-date-picker
+                        @change="(value) => changeHandler(value)"
+                        placeholder="请选择时间"
+                        v-model="valueDate"
+                        align="left"
+                        :editable="zreo"
+                        :picker-options="pickerOptions"
+                        >
+                    </el-date-picker>
+                </div>
+            </el-col>
+        </el-row>
         <el-row  type="flex" justify="space-between">
-            <el-col :span="7" class="log_height">
-                <el-col :span="14" :offset="8" style="padding-top: 12%">
-                    <div class="log_d_1">
-                       <svg class="icon log_svg" aria-hidden="true" >
-                            <use xlink:href="#el-icon-erp-num"></use>
-                        </svg>
-                    </div>
-                    <div class="log_d_2">
-                       <p class="log_p_1">5</p>
-                        <p class="log_p_2">在途货物数量</p>
-                    </div>
-                </el-col>
+            <el-col :span="7" >
+              <el-card class="log_height">
+                  <el-col :span="18" :offset="6" style="padding-top:5rem">
+                      <div class="log_d_1">
+                          <svg class="icon log_svg" aria-hidden="true" >
+                              <use xlink:href="#el-icon-erp-num"></use>
+                          </svg>
+                      </div>
+                      <div class="log_d_2">
+                          <p class="log_p_1">5</p>
+                          <p class="log_p_2">在途货物数量</p>
+                      </div>
+                  </el-col>
+              </el-card>
             </el-col>
-            <el-col :span="7" class="log_height">
-                <el-col :span="14" :offset="8" style="padding-top: 12%">
-                    <div class="log_d_1">
-                        <svg class="icon log_svg" aria-hidden="true" >
-                            <use xlink:href="#el-icon-erp-yunshu"></use>
-                        </svg>
-                    </div>
-                    <div class="log_d_2">
-                        <p class="log_p_1">5</p>
-                        <p class="log_p_2">超时运输时间</p>
-                    </div>
-                </el-col>
+            <el-col :span="7" >
+              <el-card class="log_height">
+                  <el-col :span="18" :offset="6" style="padding-top: 5rem">
+                      <div class="log_d_1">
+                          <svg class="icon log_svg" aria-hidden="true" >
+                              <use xlink:href="#el-icon-erp-yunshu"></use>
+                          </svg>
+                      </div>
+                      <div class="log_d_2">
+                          <p class="log_p_1">5</p>
+                          <p class="log_p_2">超时运输时间</p>
+                      </div>
+                  </el-col>
+              </el-card>
             </el-col>
-            <el-col :span="7" class="log_height">
-                <el-col :span="14" :offset="8" style="padding-top: 12%">
-                    <div class="log_d_1">
-                        <svg class="icon log_svg" aria-hidden="true" >
-                            <use xlink:href="#el-icon-erp-baifenbi"></use>
-                        </svg>
-                    </div>
-                    <div class="log_d_2">
-                        <p class="log_p_1">5</p>
-                        <p class="log_p_2">设置用户使用率</p>
-                    </div>
-                </el-col>
+            <el-col :span="7" >
+                <el-card class="log_height">
+                    <el-col :span="18" :offset="6" style="padding-top: 5rem">
+                        <div class="log_d_1">
+                            <svg class="icon log_svg" aria-hidden="true" >
+                                <use xlink:href="#el-icon-erp-baifenbi"></use>
+                            </svg>
+                        </div>
+                        <div class="log_d_2">
+                            <p class="log_p_1">5</p>
+                            <p class="log_p_2">设置用户使用率</p>
+                        </div>
+                    </el-col>
+                </el-card>
             </el-col>
         </el-row>
         <el-row style="margin-top: 4rem">
@@ -91,7 +112,14 @@
         data(){
         return {
             tableData:[{a:1,b:1,c:1,d:1,e:1}],
-            tableData_two:[{a:1,b:1,c:1,d:1,e:1,f:1}]
+            tableData_two:[{a:1,b:1,c:1,d:1,e:1,f:1}],
+            valueDate:new Date(),
+            zreo:false,
+            pickerOptions: {
+                disabledDate(time) {
+            return time.getTime() > Date.now();
+        }
+    },
         }
 
     },
@@ -116,5 +144,5 @@
         color:rgb(255, 255, 255);
         background-color:rgb(1, 172, 237)
     }.el-table td{height: 36px;padding: 5px 0}
-    .log_height{height: 16rem;margin-top:5rem; box-shadow: 5px 5px 3px #E5E5E5;background-color: #feffff}
+    .log_height{height: 16rem;}
 </style>
